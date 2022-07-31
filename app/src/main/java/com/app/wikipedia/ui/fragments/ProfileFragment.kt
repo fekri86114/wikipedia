c2c56134d88d0f3253a5cc3ffdd600b164b5e79e
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-07-31 17:46:35 +0430

    worked with MaterialCardView

--- app/src/main/java/com/app/wikipedia/ui/fragments/ProfileFragment.kt
+++ app/src/main/java/com/app/wikipedia/ui/fragments/ProfileFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import com.app.wikipedia.R
 import com.app.wikipedia.databinding.FragmentProfileBinding
+import com.google.android.material.card.MaterialCardView
 
 class ProfileFragment : Fragment() {
     lateinit var binding: FragmentProfileBinding
@@ -19,6 +22,21 @@ class ProfileFragment : Fragment() {
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cardView= view.findViewById<MaterialCardView>(R.id.cardview_main)
+        cardView.isChecked = true
+
+        cardView.setOnClickListener {
+
+            cardView.isChecked = !cardView.isChecked
+
+        }
+
+
     }
 
 }
