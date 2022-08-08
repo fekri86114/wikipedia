0402223693d7c28c3483b248c2ef20a46c3f7b48
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-08-08 19:28:38 +0430

    worked with collapsing modes and sat back stack

--- app/src/main/java/com/app/wikipedia/MainActivity2.kt
+++ app/src/main/java/com/app/wikipedia/MainActivity2.kt
@@ -2,6 +2,8 @@ package com.app.wikipedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
 import com.app.wikipedia.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
@@ -13,6 +15,24 @@ class MainActivity2 : AppCompatActivity() {
         setContentView( binding.root )
         setSupportActionBar( binding.toolbarMain2 )
 
+        binding.collapsingMain.setExpandedTitleColor(
+            ContextCompat.getColor( this, android.R.color.transparent )
+        )
+
+        binding.collapsingMain.title = "Universe" // you can set title with this code! or at CollapsingToolbarLayout (xml) part with app:title="Universe"
+
+        supportActionBar!!.setHomeButtonEnabled( true )
+        supportActionBar!!.setDisplayHomeAsUpEnabled( true )
 
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if ( item.itemId == android.R.id.home ) {
+            onBackPressed()
+        }
+
+        return true
+    }
+
 }
